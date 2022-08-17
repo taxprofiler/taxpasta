2e016b94084869737ff70c537b9b60209a4fa7bf
Moritz E. Beber <midnighter@posteo.net>
2022-08-17 17:22:10 +0200

    tests: move fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -21,3 +21,8 @@ import pytest
 @pytest.fixture(scope="session")
 def data_dir() -> Path:
     return Path(__file__).parent / "data"
+
+
+@pytest.fixture(scope="module")
+def kraken2_data_dir(data_dir) -> Path:
+    return data_dir / "kraken2"
