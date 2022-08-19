96b8991742a15d090ffc61ba805f604e9117f6cb
maxibor <maxime.borry@gmail.com>
2022-08-19 16:09:18 +0200

    add metaphlan

---
+++ src/taxpasta/infrastructure/application/metaphlan_profile.py
@@ -0,0 +1,57 @@
+# Copyright (c) 2022, Moritz E. Beber, Maxime Borry, Jianhong Ou, Sofia Stamouli.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+"""Provide a description of the kraken2 profile format."""
+
+
+import numpy as np
+import pandas as pd
+import pandera as pa
+from pandera.typing import Series
+from typing import Dict
+
+rank_prefixes = dict({
+            'k': 'kingdom',
+            'p': 'phylum',
+            'c': 'class',
+            'o': 'order',
+            'f': 'family',
+            'g': 'genus',
+            's': 'species',
+        })
+
+class MetaphlanProfile(pa.SchemaModel):
+    """Define the expected metaphlan profile format."""
+
+    clade_name: Series[str] = pa.Field()
+    taxonomy_id: Series[str] = pa.Field()
+    relative_abundance: Series[float] = pa.Field(ge=0.0, le=100.0)
+    additional_species: Series[str] = pa.Field()
+    rank: Series[pd.CategoricalDtype] = pa.Field(isin=rank_prefixes.values())
+
+    @classmethod
+    @pa.check("relative_abundance", groupby="rank", name="compositionality")
+    def check_compositionality(cls, grouped_value: Dict[pd.CategoricalDtype, Series[float]]) -> bool:
+        """Check that the percentages add up to a hundred."""
+        is_compositional = True
+        for r in rank_prefixes.values():
+            if not np.isclose(grouped_value[r].sum(), 100.0, atol=1.0):
+                return False
+        return is_compositional
+
+    class Config:
+        coerce = True
+        ordered = True
+        strict = True
---
+++ src/taxpasta/infrastructure/application/metaphlan_profile_reader.py
@@ -0,0 +1,58 @@
+# Copyright (c) 2022, Moritz E. Beber, Maxime Borry, Jianhong Ou, Sofia Stamouli.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+"""Provide a reader for kraken2 profiles."""
+
+
+from pathlib import Path
+
+import pandas as pd
+from pandera.typing import DataFrame
+
+from taxpasta.application import ProfileReader
+
+from .metaphlan_profile import MetaphanProfile, rank_prefixes
+
+
+class MetaphlanProfileReader(ProfileReader):
+    """Define a reader for kraken2 profiles."""
+
+    @classmethod
+    def read(cls, profile: Path) -> DataFrame[MetaphanProfile]:
+        """Read a kraken2 taxonomic profile from a file."""
+        result = pd.read_table(
+            filepath_or_buffer=profile,
+            sep="\t",
+            header=None,
+            index_col=False,
+            comment="#",
+        )
+        if len(result.columns) == 4:
+            result.columns = [
+                "clade_name",
+                "taxonomy_id",
+                "relative_abundance",
+                "additional_species",
+            ]
+        else:
+            raise ValueError(
+                f"Unexpected metaphlan report format. It has {len(result.columns)} "
+                f"columns but only 4 are expected."
+            )
+
+        result = result.assign(
+            rank =result.clade_name.str.split('|').str[-1].str.split("__").str[0].map(rank_prefixes)
+        )
+        return result
