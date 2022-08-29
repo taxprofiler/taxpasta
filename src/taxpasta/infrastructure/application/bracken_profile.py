93cf4661a1b8c4efce4e4e171e0a23aaf59ef458
Moritz E. Beber <midnighter@posteo.net>
2022-08-29 10:57:14 +0200

    feat: add ETL for Bracken

---
+++ src/taxpasta/infrastructure/application/bracken_profile.py
@@ -0,0 +1,60 @@
+# Copyright (c) 2022, Moritz E. Beber, Maxime Borry, Jianhong Ou, Sofia Stamouli.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+"""Provide a description of the Bracken profile format."""
+
+
+import numpy as np
+import pandas as pd
+import pandera as pa
+from pandera.typing import DataFrame, Series
+
+
+class BrackenProfile(pa.SchemaModel):
+    """Define the expected Bracken profile format."""
+
+    name: Series[str] = pa.Field()
+    taxonomy_id: Series[pd.CategoricalDtype] = pa.Field()
+    taxonomy_lvl: Series[pd.CategoricalDtype] = pa.Field()
+    kraken_assigned_reads: Series[int] = pa.Field(ge=0)
+    added_reads: Series[int] = pa.Field(ge=0)
+    new_est_reads: Series[int] = pa.Field(ge=0)
+    fraction_total_reads: Series[float] = pa.Field(ge=0.0, le=1.0)
+
+    @pa.check("fraction_total_reads", name="compositionality")
+    @classmethod
+    def check_compositionality(cls, fraction_total_reads: Series[float]) -> bool:
+        """Check that the fraction of reads adds up to one."""
+        # Bracken reports fractions with five decimals but rounding errors accumulate.
+        return len(fraction_total_reads) == 0 or bool(
+            np.isclose(fraction_total_reads.sum(), 1.0, atol=1e-03)
+        )
+
+    @pa.dataframe_check
+    @classmethod
+    def check_added_reads_consistency(cls, table: DataFrame) -> Series[bool]:
+        """Check that Bracken added reads are consistent."""
+        return (
+            table[BrackenProfile.kraken_assigned_reads]
+            + table[BrackenProfile.added_reads]
+            == table[BrackenProfile.new_est_reads]
+        )
+
+    class Config:
+        """Configure the schema model."""
+
+        coerce = True
+        ordered = True
+        strict = True
