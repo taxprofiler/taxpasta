73cffddb850f66119e5758106a2b05216d470dfd
Moritz E. Beber <midnighter@posteo.net>
2022-10-20 14:30:12 +0200

    docs: fix copy & paste error
    Co-authored-by: Sofia Stamouli <91951607+sofstam@users.noreply.github.com>

--- tests/unit/infrastructure/application/test_bracken_profile.py
+++ tests/unit/infrastructure/application/test_bracken_profile.py
@@ -13,7 +13,7 @@
 # limitations under the License.
 
 
-"""Test that the schema model validates kraken2 profiles correctly."""
+"""Test that the schema model validates bracken profiles correctly."""
 
 
 from typing import Collection
