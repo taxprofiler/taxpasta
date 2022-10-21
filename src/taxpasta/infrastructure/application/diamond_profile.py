c46d99c3068209788c022bf46e4966cbf1ef55a5
maxibor <maxime.borry@gmail.com>
2022-10-21 14:08:29 +0200

    add diamond

---
+++ src/taxpasta/infrastructure/application/diamond_profile.py
@@ -0,0 +1,36 @@
+# Copyright (c) 2022, Moritz E. Beber, Maxime Borry, Jianhong Ou, Sofia Stamouli.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+"""Provide a description of the diamond profile format."""
+
+
+
+import pandera as pa
+from pandera.typing import Series
+
+
+class DiamondProfile(pa.SchemaModel):
+    """Define the expected diamond profile format."""
+
+    query_id: Series[str] = pa.Field()
+    taxonomy_id: Series[str] = pa.Field()
+    e_value: Series[float] = pa.Field(ge=0.0, le=1.0)
+
+    class Config:
+        """Configure the schema model."""
+
+        coerce = True
+        ordered = True
+        strict = True
