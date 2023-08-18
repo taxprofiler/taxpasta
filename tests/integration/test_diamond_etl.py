a6d4c961561f19302492c0b535c1d760a3da6795
Moritz E. Beber <midnighter@posteo.net>
2023-08-18 18:57:53 +0200

    tests: add missing cases

--- tests/integration/test_diamond_etl.py
+++ tests/integration/test_diamond_etl.py
@@ -37,7 +37,9 @@ from taxpasta.infrastructure.application import (
         ("bracken", "2612_pe-ERR5766176_B-db1_S.tsv"),
         ("centrifuge", "AD_pe-db1.centrifuge.txt"),
         # ("diamond", "diamond_valid_1.tsv"),  # noqa: E800
+        ("ganon", "2612_pe_ERR5766176_db1.ganon.tre"),
         ("kaiju", "barcode41_se-barcode41-kaiju.txt"),
+        ("kmcp", "2612_pe_ERR5766176_db1.kmcp_profile.profile"),
         ("kraken2", "2612_pe-ERR5766176-db1.kraken2.report.txt"),
         ("krakenuniq", "test1.krakenuniq.report.txt"),
         ("megan6", "malt_rma2info_valid.txt.gz"),
